5418c8bf51648548c49b57b044c1c4178a19b862
navneet <git>
2025-09-12 00:59:56 +0530

    basic uinof social midea app

---
+++ Projects/Social-midea/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/sidebar";
+import CreatePost from "./components/CreatePost";    
+import PostList from "./components/PostList";
+
+function App() {
+  return (
+    
+      <div className='app-container'>
+        <Sidebar></Sidebar>
+        <div className="content">
+        <Header />
+        <CreatePost />
+        <PostList/>
+        <Footer></Footer>
+        </div>
+      </div>
+    
+  );
+}
+
+export default App;
