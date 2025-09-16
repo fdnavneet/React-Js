702d675e40d6500aa6588619abe06897be24f274
navneet <git>
2025-09-16 18:14:36 +0530

    react-dedux

---
+++ Redux-React/counter-react-redux/src/store/index.js
@@ -0,0 +1,39 @@
+import {createStore} from "redux";
+
+const INITIAL_VALUE ={
+  counter: 0,
+  Privacy :false
+}
+
+const counterReducer= (store = INITIAL_VALUE,action)=>{
+  if (action.type === "INCREMENT" ){
+    return(
+      {...store,counter : store.counter + 1}
+    )
+  } else if (action.type === "DECREMENT"){
+    return(
+      {...store,counter : store.counter - 1 }
+    )
+  } else if (action.type === "ADD"){
+    return(
+      {...store,counter : store.counter + Number(action.payload.num)}
+    )
+  }else if (action.type === "SUBTRACT"){
+    return(
+      {...store,counter : store.counter - Number(action.payload.num)}
+    )
+  }
+  else if (action.type === "PRIVACY_TOGGLE"){
+    return(
+      {...store, Privacy: !store.Privacy}
+    )
+  }
+  
+  return(
+    store
+  )
+};
+
+const counterStore=createStore(counterReducer);
+
+export default counterStore;
